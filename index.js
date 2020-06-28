ffb64f1a74c9dee195cf2135f2c2140412b87297
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-06-28 19:14:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function add(a,b){
-  var a, b;
-  a += b;
-  return a;
+  //var a, b;
+  //a += b;
+  //return a;
   // i'm not sure what's actually going on here. feel like for clarity it should be more like
   /*
   var intA = a, intB = b;
   intA += intB;
   return intA;
   */
+  var intA = a, intB = b;
+  intA += intB;
+  return intA;
 }
 function subtract(a,b){
   var a, b;
